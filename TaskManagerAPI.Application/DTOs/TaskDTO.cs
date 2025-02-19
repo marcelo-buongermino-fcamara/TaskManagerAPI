b3c554a2f6242b18c5675b977c1373b7979190e7
buongermino <buon.marcelo@outlook.com>
2025-02-19 17:12:12 -0300

    feat: add DTO

---
+++ TaskManagerAPI.Application/DTOs/TaskDTO.cs
@@ -0,0 +1,19 @@
+﻿using TaskManagerAPI.Domain.Enums;
+
+namespace TaskManagerAPI.Application.DTOs;
+
+public class TaskRequest
+{
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public DateTime? ExpiresIn { get; set; }
+    public Status Status { get; set; }
+}
+
+public record TaskResponse
+{ 
+    public string Title { get;}
+    public string? Description { get;}
+    public DateTime? ExpiresIn { get;}
+    public Status Status { get;}
+}
