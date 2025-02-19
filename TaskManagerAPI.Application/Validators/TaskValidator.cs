b9d963934eb3d87629e05c90a0e33b16334e5c80
buongermino <buon.marcelo@outlook.com>
2025-02-19 17:12:53 -0300

    feat: add package fluent validation and add validator

---
+++ TaskManagerAPI.Application/Validators/TaskValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using TaskManagerAPI.Application.DTOs;
+
+namespace TaskManagerAPI.Application.Validators;
+
+public class TaskValidator : AbstractValidator<TaskRequest>
+{
+    public TaskValidator()
+    {
+        RuleFor(p => p.Title)
+            .NotEmpty()
+            .WithMessage("Title is required")
+            .MaximumLength(50)
+            .WithMessage("Title must have a maximum of 50 characters");
+
+        RuleFor(p => p.Description)
+            .MaximumLength(100)
+            .WithMessage("Description must have a maximum of 200 characters");
+    
+        RuleFor(p => p.ExpiresIn)
+            .GreaterThan(DateTime.Now)
+            .WithMessage("ExpiresIn must be greater than the current date");
+
+        RuleFor(x => x.Status).IsInEnum()
+            .WithMessage("Invalid status");
+    }
+}
