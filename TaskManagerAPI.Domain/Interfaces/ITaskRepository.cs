e982971b5699b1d6988128fdc3e87c605cac7a3c
buongermino <buon.marcelo@outlook.com>
2025-02-19 17:51:31 -0300

    feat: add repository interface

---
+++ TaskManagerAPI.Domain/Interfaces/ITaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using TaskManagerAPI.Domain.Enums;
+
+namespace TaskManagerAPI.Domain.Interfaces;
+
+public interface ITaskRepository
+{
+    Task<IEnumerable<Task>> GetAllAsync(Status? status, DateTime? expiresIn);
+    Task<Task> CreateAsync(Task task);
+    Task<Task> UpdateAsync(Task task);
+    Task<Task> DeleteAsync(Guid id);
+}
