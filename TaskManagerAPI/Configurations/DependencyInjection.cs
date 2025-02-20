a39f65d189607258b6883f892c6104f4d883f109
buongermino <buon.marcelo@outlook.com>
2025-02-20 15:40:27 -0300

    feat: clean unused files, add DependencyInjection class to handle services

---
+++ TaskManagerAPI/Configurations/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Application.UseCases;
+using TaskManagerAPI.Domain.Interfaces;
+using TaskManagerAPI.Infrastructure;
+using TaskManagerAPI.Infrastructure.Repositories;
+
+namespace TaskManagerAPI.API.Configurations;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        //Application
+        services.AddScoped<ICreateTask, CreateTask>();
+        services.AddScoped<IUpdateTask, UpdateTask>();
+        services.AddScoped<IDeleteTask, DeleteTask>();
+        services.AddScoped<IListTasks, ListTasks>();
+
+        //Domain
+        services.AddScoped<ITaskRepository, TaskRepository>();
+
+        //Infrastructure
+        services.AddDbContext<ApiContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
+
+        return services;
+    }
+}
