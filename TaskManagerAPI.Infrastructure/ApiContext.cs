ec1450e173ea4869a33cf613a9deaa934f26ec16
buongermino <buon.marcelo@outlook.com>
2025-02-20 15:38:59 -0300

    feat: add db context and repository implementation class

---
+++ TaskManagerAPI.Infrastructure/ApiContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Domain.Entities;
+
+namespace TaskManagerAPI.Infrastructure;
+
+public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
+{
+    public DbSet<ToDoItem> ToDoItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ToDoItem>().HasKey(x => x.ID);
+        modelBuilder.Entity<ToDoItem>().Property(x => x.Title).IsRequired();
+        modelBuilder.Entity<ToDoItem>().Property(x => x.Description).IsRequired(false);
+        modelBuilder.Entity<ToDoItem>().Property(x => x.ExpiresIn).IsRequired(false);
+        modelBuilder.Entity<ToDoItem>().Property(x => x.Status).IsRequired(false);
+        modelBuilder.Entity<ToDoItem>().Property(x => x.CreatedAt).IsRequired();
+        modelBuilder.Entity<ToDoItem>().Property(x => x.UpdatedAt).IsRequired();
+    }
+}
---
+++ TaskManagerAPI.Infrastructure/Repositories/TaskRepository.cs
@@ -0,0 +1,32 @@
+﻿using TaskManagerAPI.Domain.Entities;
+using TaskManagerAPI.Domain.Enums;
+using TaskManagerAPI.Domain.Interfaces;
+
+namespace TaskManagerAPI.Infrastructure.Repositories;
+
+public class TaskRepository(ApiContext context) : ITaskRepository
+{
+    private readonly ApiContext _context = context;
+
+    public async Task CreateAsync(ToDoItem task)
+    {
+        _context.ToDoItems.Add(task);
+        await _context.SaveChangesAsync();
+    }
+
+    public Task<ToDoItem> DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<ToDoItem>> GetAllAsync(Status? status, DateTime? expiresIn)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ToDoItem> UpdateAsync(ToDoItem task)
+    {
+        throw new NotImplementedException();
+    }
+}   
+    
