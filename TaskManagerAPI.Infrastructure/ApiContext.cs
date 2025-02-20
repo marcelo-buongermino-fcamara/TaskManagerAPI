3486a7fd8af895ac1c9212afc6411428cc4eae42
buongermino <buon.marcelo@outlook.com>
2025-02-20 20:45:20 -0300

    feat: improve context to populate db when run app

--- TaskManagerAPI.Infrastructure/ApiContext.cs
+++ TaskManagerAPI.Infrastructure/ApiContext.cs
@@ -3,8 +3,13 @@ using TaskManagerAPI.Domain.Entities;
 
 namespace TaskManagerAPI.Infrastructure;
 
-public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(options)
+public class ApiContext : DbContext
 {
+    public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+    {
+        PopulateDB();
+    }
+
     public DbSet<ToDoItem> ToDoItems { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -13,8 +18,27 @@ public class ApiContext(DbContextOptions<ApiContext> options) : DbContext(option
         modelBuilder.Entity<ToDoItem>().Property(x => x.Title).IsRequired();
         modelBuilder.Entity<ToDoItem>().Property(x => x.Description).IsRequired(false);
         modelBuilder.Entity<ToDoItem>().Property(x => x.ExpiresIn).IsRequired(false);
-        modelBuilder.Entity<ToDoItem>().Property(x => x.Status).IsRequired(false);
+        modelBuilder.Entity<ToDoItem>().Property(x => x.Status);
         modelBuilder.Entity<ToDoItem>().Property(x => x.CreatedAt).IsRequired();
         modelBuilder.Entity<ToDoItem>().Property(x => x.UpdatedAt).IsRequired();
     }
+
+    public void PopulateDB()
+    {
+        if (!ToDoItems.Any())
+        {
+            var tasks = new List<ToDoItem>
+            {
+                new("Task 1", "Description 1", DateTime.UtcNow.AddDays(7), Domain.Enums.Status.Pending),
+                new("Task 2", "Description 2", DateTime.UtcNow.AddDays(14), Domain.Enums.Status.InProgress),
+                new("Task 3", "Description 3", DateTime.UtcNow.AddDays(21), Domain.Enums.Status.Done),
+                new("Task 4", "Description 4", DateTime.UtcNow.AddDays(2), Domain.Enums.Status.Pending),
+                new("Task 5", "Description 5", DateTime.UtcNow.AddDays(1), Domain.Enums.Status.InProgress),
+            };
+
+            ToDoItems.AddRange(tasks);
+            SaveChanges();
+        }
+    }
+    
 }
