aafb2f72bf366be9b2d049feb4bf7e15215d60a8
buongermino <buon.marcelo@outlook.com>
2025-02-21 16:31:42 -0300

    test: add Test ToDTO method

---
+++ TaskManagerAPI.Test/DTOs/ToDoItemResponseToDTO.cs
@@ -0,0 +1,26 @@
+﻿using TaskManagerAPI.Application.DTOs;
+using TaskManagerAPI.Domain.Entities;
+using TaskManagerAPI.Domain.Enums;
+
+namespace TaskManagerAPI.Tests.DTOs;
+
+public class ToDoItemResponseToDTO
+{
+    [Fact]
+    public void ToDTO_ShouldConvertToDoItemToToDoItemResponse()
+    {
+        // Arrange
+        var toDoItem = new ToDoItem("Test Title", "Test Description", DateTime.Now.AddDays(1), Status.Pending);
+
+        // Act
+        var toDoItemResponse = ToDoItemResponse.ToDTO(toDoItem);
+
+        // Assert
+        Assert.Equal(toDoItem.ID, toDoItemResponse.ID);
+        Assert.Equal(toDoItem.Title, toDoItemResponse.Title);
+        Assert.Equal(toDoItem.Description, toDoItemResponse.Description);
+        Assert.Equal(toDoItem.ExpiresIn, toDoItemResponse.ExpiresIn);
+        Assert.Equal(toDoItem.Status.ToString(), toDoItemResponse.Status);
+    }
+}
+
